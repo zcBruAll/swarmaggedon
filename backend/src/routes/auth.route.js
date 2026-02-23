3b589750d1b41548382d6a498f01487868dee973
g-ae <goncalo.arieira@hotmail.com>
2026-02-23 13:05:31 +0100

    fixed logout function wrong name

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,10 +1,10 @@
 import {Router} from 'express'
-import { postLogin, postRegister, logout } from '../controllers/auth.controller.js'
+import { postLogin, postRegister, deleteLogout } from '../controllers/auth.controller.js'
 
 const router = Router()
 
 router.route("/login").post(postLogin)
 router.route("/register").post(postRegister)
-router.route("/logout").delete(logout)
+router.route("/logout").delete(deleteLogout)
 
 export default router
