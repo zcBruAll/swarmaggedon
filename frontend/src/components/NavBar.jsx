cc7944e67c0349fb5a2c1c52a66d8369635b6aa1
g-ae <goncalo.arieira@hotmail.com>
2026-02-27 23:30:57 +0100

    added indicator for pending friend requests

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,10 +1,21 @@
 import { Link, useLocation } from 'react-router-dom';
 import '../assets/style/components/NavBar.css';
 import { useAuth } from '../context/AuthContext';
+import { gql } from '@apollo/client';
+import { useQuery } from '@apollo/client/react';
+
+const PENDING_REQUESTS = gql`
+  query PR {
+    pending_requests {
+      id
+    }
+  }
+`
 
 function NavBar() {
   const location = useLocation();
   const { isLoggedIn, user } = useAuth();
+  const { loading, error, data } = useQuery(PENDING_REQUESTS)
   
   return (
     <header>
@@ -33,6 +44,7 @@ function NavBar() {
               className={`nav-link ${location.pathname === '/friends' ? 'active' : ''}`}
             >
               Friends
+              {!loading && !error && data.pending_requests?.length > 0 ? <span className="nav-badge">{data.pending_requests.length}</span>: ""}
             </Link>
           </>
         }
