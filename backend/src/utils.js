2db7b27a1e1715af6c183fb4908fc1ab7b4cd6cb
g-ae <goncalo.arieira@hotmail.com>
2026-03-05 13:39:54 +0100

    added '_' to list of accepted chars for username

--- backend/src/utils.js
+++ backend/src/utils.js
@@ -29,7 +29,9 @@ export const checkOnlyAlphanumeric = (str) => {
         code = str.charCodeAt(i);
         if (!(code > 47 && code < 58) && // numeric (0-9)
             !(code > 64 && code < 91) && // upper alpha (A-Z)
-            !(code > 96 && code < 123)) { // lower alpha (a-z)
+            !(code > 96 && code < 123) && // lower alpha (a-z)
+            !(code == 95) // _
+        ) { 
             return false;
         }
     }
