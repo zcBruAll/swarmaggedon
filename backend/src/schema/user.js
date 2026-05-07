f5d8482f64fe127782d07082e48c7362e9857b4d
g-ae <goncalo.arieira@hotmail.com>
2026-05-07 13:08:07 +0200

    fix(backend): Fixed cookie given without sameSite property

--- backend/src/schema/user.js
+++ backend/src/schema/user.js
@@ -62,6 +62,7 @@ export const userResolvers = {
             res.cookie('auth_token', token, {
                 httpOnly: true,
                 secure: process.env.NODE_ENV === 'production',
+                sameSite: 'lax',
                 maxAge: 7 * 24 * 60 * 60 * 1000
             });
 
